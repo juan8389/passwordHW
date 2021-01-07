fc9dab4a94af468d2f24ae7a89655a11860e241e
Juan Santoyo <juan8389@gmail.com>
2021-01-07 17:17:58 -0500

    transferring some data to js

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ console.log("Hello world!")
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var resultEl = document.getElementById('results');
+var lowercaseEl = document.getElementById('lowercase');
+var uppercaseEl = document.getElementById('uppercase');
+var numbersEl = document.getElementById('numbers');
+var symbolsEl = document.getElementById('symbols');
+var generateEl = document.getElementById('generate');
+var lengthEl = document.getElementById('length');
+
+// Write password to the #password input 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -11,6 +19,32 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+var randomFunc = {
+    lower: getRandomLower
+    upper: getRandomUpper
+    number: getRandomNumber
+    symbol: getRandomSymbol
+
+}
+function myFunction() {
+    var text;
+    var favDrink = prompt("What's your favorite cocktail drink?", "Daiquiri");
+
+let lower = prompt ("Do you want lowercase on your password"):
+
+if (lower.toLowerCase() == "yes") {
+    alert("I will add lowercase")
+} 
+
+generateEl.addEventListener('click', () => {
+    var length = +lengthEl.value;
+    var hasLower = lowercaseEl. checked;
+    var hasLower = uppercaseEl. checked;
+    var hasLower = numbersEl. checked;
+    var hasLower = symbolsEl. checked;
+
+})
 
 function getRandomLower(){
     return String.fromCharCode(Math.floor(Math.random() *26) + 97);
@@ -27,7 +61,6 @@ function getRandomNumber(){
 function getRandomSymbol(){
     var symbols = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
     return symbols[Math.floor(Math.random() * symbols.length)];
-
 }
 
 // console.log(getRandomSymbol());
