dc45f980d6d281bd308a1d27f115d0404451a766
Juan Santoyo <juan8389@gmail.com>
2021-01-09 23:53:59 -0500

    last minute changes

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # passwordHW
+
+## Table of Contents
+
+* [Description](#Description)
+* [Challenges](#Challenges)
+* [Tools](#Tools)
+
+## Description 
+
+This Assignment was really challenging I had to learn how to use JavaScript to assign functions, Eventlisteners, and many more terms that were new to me to my application. I had a lot of fun creating this password generator because I was able to put to use some of the things that we have learn in class into one project.
+
+![Password Generator ScreenShot](Password-Generator-ScreenShot.png)
+https://juan8389.github.io/passwordHW/
+ 
+
+## Challenges
+
+This assignment was challenging and fun. This homework used Javascript, HTML, and CSS. I was able to research and develop this application.
+
+
+## Tools 
+
+I used tutoring, getbootstrap and W3schools most important my notes and class work.
--- script.js
+++ script.js
@@ -1,76 +1,135 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var upperCaseRandom = "";
+var lowerCaseRandom = "";
+var myNumbers = "";
+var mySpecials = "";
+var field = document.getElementById('password-1');
+var myBtn = document.getElementById('generate');
 
-var greeting = alert("Hello and welcome to creating your secured password");
 
-var uppercaseEl = prompt("Would you like uppercase in your password?");
-    if (uppercaseEl=='yes'){
-        alert("Great I will add uppercase to your password");
-    }
-    if (uppercaseEl=='no'){
-        alert("I will not add uppercase to your password");
-    }
-    console.log(uppercaseEl.toString());  
+//  -------------------Uppercase characters-------------------
 
-var lowercaseEl = prompt("Would you like lowercase in your password?");
-    if (lowercaseEl=='yes'){
-        alert("Great I will add lowercase to your password");
-    }
-    if (lowercaseEl=='no'){
-        alert("I will not add lowercase to your password");
-    }
-    console.log(lowercaseEl.toString());  
 
-var symbolsEl = prompt("Would you like symbols in your password?");
-    if (symbolsEl=='yes'){
-        alert("Great I will add symbols to your password");
-    }
-    if (symbolsEl=='no'){
-        alert("I will not add symbols to your password");
-    }
-    console.log(symbolsEl.toString());  
+function myUpper(n) {
+    while (upperCaseRandom.length < n) {
+        upperCaseRandom += String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    };
 
-var numbersEl = prompt("Would you like numbers in your password?");
-    if (numbersEl=='yes'){
-        alert("Great I will add numbers to your password");
-    }
-    if (numbersEl=='no'){
-        alert("I will not add numbers to your password");
-    }
-    console.log(numbersEl.toString());  
+};
 
-var lengthEl = prompt("How long do you want your password from 8-128 characters?",0);
-    if (lengthEl.between > 8 && lengthEl < 128);
-    while (lengthEl.between > 128 && lengthEl < ~8)
-        
-var passwordLength = lengthEl;
+//  -------------------Special characters-------------------
 
-    console.log(lengthEl.toString());  
+function specialChar(f) {
+    var specialChar = "!@#$%^&*()_+=-?";
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    while (mySpecials.length < f) {
+        mySpecials += specialChar[Math.floor(Math.random() * specialChar.length)];
+    };
+    console.log(mySpecials);
+};
 
-  passwordText.value = password;
+//  to shuffle the final password
+function getRandomPassword(d) {
+    var arr = d.split('');
+    arr.sort(() => {
+        return 0.5 - Math.random();
+    });
+    password = arr.join('');
+    return password;
+};
 
+
+// ------------------lowercase-------------------
+function myLower(t) {
+    while (lowerCaseRandom.length < t) {
+        lowerCaseRandom += String.fromCharCode(97 + Math.floor(Math.random() * 26));
+    };
+    console.log(lowerCaseRandom);
+};
+
+// ----------------------numbers-------------------
+function numbersRan(r) {
+    var numbers = "1234567890";
+
+    while (myNumbers.length < r) {
+        myNumbers += numbers[Math.floor(Math.random() * numbers.length)];
+    };
+    console.log(myNumbers);
 }
 
-function getRandomLower(){
-    return String.fromCharCode(Math.floor(Math.random() *26) + 97);
-}
 
-function getRandomUpper(){
-    return String.fromCharCode(Math.floor(Math.random() *26) + 65);
-}
+//main button function //
+function deploy() {
+    // to get a number value from user  
+    var passwordLength = prompt('How long do you want your password from 8-128 characters?');
+    console.log("Value of password is: " + passwordLength);
+    // convert string value to number
+    passwordLength = parseInt(passwordLength);
+    // check value is number 
+    if (isNaN(passwordLength)) {
+        //  to return home if value is not a number
+        alert('input not valid');
+        location.reload();
+        return false;
+    } else {
+        if (passwordLength >= 8 && passwordLength <= 128) {
+            //  execute functions to get random values}
+            myUpper(passwordLength);
+            specialChar(passwordLength);
+            myLower(passwordLength);
+            numbersRan(passwordLength);
+        }
+        else {
+            alert('input not valid');
+            location.reload();
+            return false;
+        }
+    };
 
-function getRandomNumber(){
-    return String.fromCharCode(Math.floor(Math.random() *26) + 48);
-}
+    var useUpper = confirm('Do you want to use uppercase?');
+    if (useUpper == true) {
+        upperCaseRandom = upperCaseRandom;
+    }
+    else {
+        upperCaseRandom = '';
+    };
 
-function getRandomSymbol(){
-    var symbols = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    var useSpecials = confirm("Do you want to use special characters?");
+    if (useSpecials == true) {
+        mySpecials = mySpecials;
+    }
+    else {
+        mySpecials = '';
+    }
+
+    var useLower = confirm('Do you want lowercase?');
+    if (useLower == true) {
+        lowerCaseRandom = lowerCaseRandom;
+    }
+    else {
+        lowerCaseRandom = '';
+    };
+
+    var useNumbers = confirm("Do you want to use numbers?");
+    if (useNumbers == true) {
+        myNumbers = myNumbers;
+    }
+    else {
+        myNumbers = '';
+    }
+    var password = upperCaseRandom + mySpecials + lowerCaseRandom + myNumbers;
+    // shuffle the values 
+    var password2 = getRandomPassword(password);
+    // to get final Password length based on user preferences
+    var finalPassword = password2.slice(0, passwordLength);
+    // display password 
+    field.innerHTML = finalPassword;
+
+};
+
+// call function
+myBtn.addEventListener('click', deploy);
+
+
+// var password = lowerCaseRandom + upperCaseRandom + mySpecials + myNumbers;
+// console.log("this is before shuffle: " + password);
+
